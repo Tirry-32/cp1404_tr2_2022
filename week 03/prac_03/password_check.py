8496d1ee9d79f48a7ee02d25af8eaef75fa83fee
Tirivashe <tirivasheignatious.ushamba@my.jcu.edu.au>
2022-06-15 19:47:29 +1000

    Refactor password check program to use functions

--- week 03/prac_03/password_check.py
+++ week 03/prac_03/password_check.py
@@ -1,12 +1,18 @@
+"""
+CP1404 - Practical
+Get a password with minimum length and display asterisks.
+"""
 MINIMUM_LENGTH = 9
 
 
 def main():
+    """Get user password and print in as aterisks."""
     password = get_password(MINIMUM_LENGTH)
     print('*' * len(password))
 
 
 def get_password(minimum_length):
+    """Get user password."""
     password = input(f"Please enter your password of minimum length {minimum_length}: ")
     while len(password) < minimum_length:
         print("Your Entered Password is too short")
