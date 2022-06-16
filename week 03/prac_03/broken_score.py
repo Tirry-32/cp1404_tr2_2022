f4cf952f6686f5946ecf2f315dc89c7d76b19499
Tirivashe <tirivasheignatious.ushamba@my.jcu.edu.au>
2022-06-16 13:24:51 +1000

    version 1.0

---
+++ week 03/prac_03/broken_score.py
@@ -0,0 +1,17 @@
+
+"""
+CP1404 - Practical
+Broken program to determine score status
+"""
+
+# TODO: Fix this!
+
+score = float(input("Enter score: "))
+if 90 <= score <= 100:
+    print("Excellent")
+elif 90 > score >= 50:
+    print("Passable")
+elif 50 > score >= 0:
+    print("Bad")
+else:
+    print("Invalid score")
