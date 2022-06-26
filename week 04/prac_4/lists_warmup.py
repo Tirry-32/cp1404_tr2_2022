df97d45fb56e27031025d7020d53c426b1f1bc76
Tirivashe <tirivasheignatious.ushamba@my.jcu.edu.au>
2022-06-26 16:20:58 +1000

    list warmup update

--- week 04/prac_4/lists_warmup.py
+++ week 04/prac_4/lists_warmup.py
@@ -11,7 +11,7 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 # False
 # False
 # [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
-numbers[0] = "10"
+numbers[0] = "ten"
 numbers[-1] = 1
 new_numbers = numbers[2:]
 kona = 9 in numbers
