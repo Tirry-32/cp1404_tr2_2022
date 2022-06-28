10ef3bea3790e9b70f1965161b9711b34006159d
Tirry-32 <107027509+Tirry-32@users.noreply.github.com>
2022-06-28 16:33:38 +1000

    removed space on nt and wa

--- week 05/prac_5/state_names.py
+++ week 05/prac_5/state_names.py
@@ -5,7 +5,7 @@ File needs reformatting
 """
 
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
-CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT" : "Northern Territory", "WA" : "Western Australia", "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia", "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
 
 state_code = input("Enter short state: ")
