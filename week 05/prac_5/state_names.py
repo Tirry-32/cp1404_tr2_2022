b9b828c54d1a3d16f5e83ac36582d13ae3850e8a
Tirivashe <tirivasheignatious.ushamba@my.jcu.edu.au>
2022-07-03 22:14:45 +1000

    removed the formatting errors

--- week 05/prac_5/state_names.py
+++ week 05/prac_5/state_names.py
@@ -1,11 +1,10 @@
 """
-CP1404/CP5632 Practical
 State names in a dictionary
 File needs reformatting
 """
 
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
-CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia", "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
 
 state_code = input("Enter short state: ")
