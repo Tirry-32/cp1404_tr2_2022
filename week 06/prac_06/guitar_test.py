90cbc28550ef8edbef7f9595e14e267c4e0cfe04
Tirivashe <tirivasheignatious.ushamba@my.jcu.edu.au>
2022-07-17 18:40:31 +1000

    version 1.0

---
+++ week 06/prac_06/guitar_test.py
@@ -0,0 +1,27 @@
+"""
+CP1404 Practical
+Basic manual tests for Guitar class
+"""
+from guitar import Guitar
+
+CURRENT_YEAR = 2017
+
+
+def run_tests():
+    """Tests for Guitar class."""
+    name = "Gibson L-5 CES"
+    year = 1922
+    cost = 16035.40
+
+    guitar = Guitar(name, year, cost)
+    other = Guitar("Another Guitar", 2012, 1512.9)
+
+    print(f"{guitar.name} get_age() - Expected { 95}. Got {guitar.get_age()}")
+    print(f"{other.name} get_age() - Expected {5}. Got {other.get_age()}")
+    print()
+    print(f"{guitar.name} is_vintage() - Expected {True}. Got {guitar.is_vintage()}")
+    print(f"{other.name} is_vintage() - Expected {False}. Got {other.is_vintage()}")
+
+
+if __name__ == '__main__':
+    run_tests()
---
+++ week 06/prac_06/guitars.py
@@ -0,0 +1,38 @@
+"""
+CP1404 Practical
+Guitar program.
+"""
+from guitar import Guitar
+
+
+def main():
+    """Guitar program, using Guitar class."""
+    guitars = []
+
+    print("My guitars!")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar_to_add = Guitar(name, year, cost)
+        guitars.append(guitar_to_add)
+        print(guitar_to_add, "added.")
+        name = input("Name: ")
+
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+    if guitars:
+        guitars.sort()
+        print("These are my guitars:")
+        for i, guitar in enumerate(guitars):
+            vintage_string = ""
+            if guitar.is_vintage():
+                vintage_string = " (vintage)"
+            print(
+                "Guitar {0}: {1.name:>20} ({1.year}), worth ${1.cost:10,.2f}{2}".format(i + 1, guitar, vintage_string))
+    else:
+        print("No guitars :( Quick, go and buy one!")
+
+
+main()
