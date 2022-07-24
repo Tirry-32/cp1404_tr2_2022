925e3041a5f99caa13e54805ea97139609e2b15d
Tirivashe <tirivasheignatious.ushamba@my.jcu.edu.au>
2022-07-24 22:31:20 +1000

    version 1.0

--- week 07/prac_07/box_layout_demo.py
+++ week 07/prac_07/box_layout_demo.py
@@ -1,3 +1,7 @@
+"""
+CP1404 Practical
+GUI program Box Layout
+"""
 from kivy.app import App
 from kivy.lang import Builder
 
---
+++ week 07/prac_07/squaring.py
@@ -0,0 +1,31 @@
+"""
+CP1404 Practical
+GUI program Squaring
+"""
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+__author__ = 'Tirivashe Ushamba'
+
+
+class SquareNumberApp(App):
+    """ SquareNumberApp is a Kivy App for squaring a number """
+    def build(self):
+        """ build the Kivy app from the kv file """
+        Window.size = (200, 100)
+        self.title = "Square Number"
+        self.root = Builder.load_file('squaring.kv')
+        return self.root
+
+    def handle_calculate(self, value):
+        """Handle calculation output result to label widget """
+        try:
+            result = float(value) ** 2
+            self.root.ids.output_label.text = str(result)
+        except ValueError:
+            pass
+
+
+SquareNumberApp().run()
